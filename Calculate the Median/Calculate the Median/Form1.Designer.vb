14d8828d088913815b9c3fb49428035106d93f4c
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-04-09 23:02:58 -0400

    Calculate the Median

---
+++ Calculate the Median/Calculate the Median/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fromCalMedian
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblArray = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblMedian = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(170, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Randomly Generated Integer Array"
+        '
+        'lblArray
+        '
+        Me.lblArray.AutoSize = True
+        Me.lblArray.Location = New System.Drawing.Point(34, 64)
+        Me.lblArray.Name = "lblArray"
+        Me.lblArray.Size = New System.Drawing.Size(39, 13)
+        Me.lblArray.TabIndex = 1
+        Me.lblArray.Text = "Label2"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(37, 110)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(173, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Median Value"
+        '
+        'lblMedian
+        '
+        Me.lblMedian.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMedian.Location = New System.Drawing.Point(267, 119)
+        Me.lblMedian.Name = "lblMedian"
+        Me.lblMedian.Size = New System.Drawing.Size(100, 23)
+        Me.lblMedian.TabIndex = 4
+        Me.lblMedian.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'fromCalMedian
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(410, 261)
+        Me.Controls.Add(Me.lblMedian)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblArray)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "fromCalMedian"
+        Me.Text = "Calculate the Median"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblArray As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblMedian As Label
+End Class
---
+++ Calculate the Median/Calculate the Median/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class fromCalMedian
+    Const intMAX_SUBSCRIPT As Integer = 5 ' The maximum subscript
+    Dim intNumbers(intMAX_SUBSCRIPT) As Integer ' Array declaration
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'determine the position of the median value is:
+        Dim medianPosition As Integer = ((intNumbers.Length + 1) / 2) - 1
+
+        'Calculate median value
+        Dim medianValue As Integer = intNumbers(medianPosition)
+
+        'Show the median value
+        lblMedian.Text = medianValue.ToString()
+
+    End Sub
+
+    Private Sub fromCalMedian_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Create an array to hold five lottery numbers.
+        'Const intMAX_SUBSCRIPT As Integer = 5 ' The maximum subscript
+        'Dim intMAX_SUBSCRIPT As Integer ' The maximum subscript
+
+        Dim intCount As Integer ' Loop counter
+
+        'Create a random object
+        Dim rand As New Random
+
+        ' Fill the array with random numbers. Each number will be in the range 0-99.
+        For intCount = 0 To intMAX_SUBSCRIPT
+            intNumbers(intCount) = rand.Next(100)
+        Next
+
+        'SOrt the array 
+        Array.Sort(intNumbers)
+
+
+        ' Display the array elements in the labels.
+        lblArray.Text = intNumbers(0).ToString() + ", " + intNumbers(1).ToString() + ", " + intNumbers(2).ToString() + ", " + intNumbers(3).ToString() + ", " + intNumbers(4).ToString()
+
+    End Sub
+End Class
